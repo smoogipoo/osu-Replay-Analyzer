65d704fb6abaf13712c9d552daf501a6aa90195e
smoogipooo <smoogipooo@gmail.com>
2014-05-04 19:12:04 +0930

    (*) Added MapInfoPlugin and ReplayInfoPlugin to main solution.
    (*) Created a new project for Spinner RPM plugin.

---
+++ SpinnerRPMPlugin/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpinnerRPMPlugin
+{
+    public class Class1
+    {
+    }
+}

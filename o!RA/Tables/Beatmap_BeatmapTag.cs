18f2f356ccd1a1048dadfb2bbd88a92ed230ee59
Condone <dilmher@gmail.com>
2014-05-11 09:31:53 +0200

    (*) Include missing table classes

---
+++ o!RA/Tables/Beatmap_BeatmapTag.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace o_RA.Tables
+{
+    class Beatmap_BeatmapTag
+    {
+        public Beatmap_BeatmapTag()
+        {
+        }
+
+        [UniqueIdentifier]
+        public int? Beatmap_BeatmapTag_Id { get; set; }
+        public int? Beatmap_Id { get; set; }
+        public int? BeatmapTag_Id { get; set; }
+    }
+}

24ab508a74c31fd1c6f3749ed54baafa7a7caca5
smoogipooo <smoogipooo@gmail.com>
2014-05-29 07:49:12 +0930

    (*) Try to fix loading of replays saved with the API.

--- osuReplayAPI/osuReplayAPI/LZMACoder.cs
+++ osuReplayAPI/osuReplayAPI/LZMACoder.cs
@@ -9,7 +9,6 @@ namespace ReplayAPI
     {
         //These properties are straight copy from the SDK.
         //Actually, I don't know what these mean.
-        private static Int32 dictionary = 1 << 21; //No dictionary
         private static Int32 posStateBits = 2;
         private static Int32 litContextBits = 3;   // for normal files  // UInt32 litContextBits = 0; // for 32-bit data                                             
         private static Int32 litPosBits = 0;       // UInt32 litPosBits = 2; // for 32-bit data
@@ -17,18 +16,14 @@ namespace ReplayAPI
 
         private static CoderPropID[] propIDs = 
     {
-        CoderPropID.DictionarySize,
         CoderPropID.PosStateBits,  
         CoderPropID.LitContextBits,
         CoderPropID.LitPosBits,
         CoderPropID.Algorithm,
         CoderPropID.NumFastBytes,
-        CoderPropID.MatchFinder,
-        CoderPropID.EndMarker
     };
         private static object[] properties = 
     {
-        (Int32)(dictionary),
         (Int32)(posStateBits),  
         (Int32)(litContextBits),
         (Int32)(litPosBits),
@@ -60,33 +55,27 @@ namespace ReplayAPI
         public MemoryStream Decompress(MemoryStream inStream, bool closeInStream)
         {
             inStream.Position = 0;
-            var outStream = new MemoryStream();
-
-            var prop = new byte[5];
-            if (inStream.Read(prop, 0, 5) != 5)
-                throw (new Exception("input .lzma is too short"));
 
             var decoder = new Decoder();
+
+            byte[] prop = new byte[5];
+            if (inStream.Read(prop, 0, 5) != 5) { }
             decoder.SetDecoderProperties(prop);
-
             long outSize = 0;
-
-            if (BitConverter.IsLittleEndian)
+            for (int i = 0; i < 8; i++)
             {
-                for (int i = 0; i < 8; i++)
-                {
-                    int v = inStream.ReadByte();
-                    if (v < 0)
-                        throw (new Exception("Can't Read 1"));
-
-                    outSize |= ((long) (byte) v) << (8*i);
-                }
+                int v = inStream.ReadByte();
+                if (v < 0)
+                    break;
+                outSize |= ((long)(byte)v) << (8 * i);
             }
-
             long compressedSize = inStream.Length - inStream.Position;
-            decoder.Code(inStream, outStream, compressedSize, outSize, null);
 
+            var outStream = new MemoryStream();
+            decoder.Code(inStream, outStream, compressedSize, outSize, null);
+            outStream.Flush();
             outStream.Position = 0;
+
             if (closeInStream)
                 inStream.Close();
 
@@ -106,6 +95,9 @@ namespace ReplayAPI
             var encoder = new Encoder();
             encoder.SetCoderProperties(propIDs, properties);
             encoder.WriteCoderProperties(outStream);
+
+            byte[] lengthHeader = BitConverter.GetBytes(inStream.Length);
+            outStream.Write(lengthHeader, 0, lengthHeader.Length);
 
             encoder.Code(inStream, outStream, -1, -1, null);
             outStream.Position = 0;

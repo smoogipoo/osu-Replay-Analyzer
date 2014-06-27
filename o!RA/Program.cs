0c5809806d36920b2feb5d33a76c15b7828ab2ad
Condone <dilmher@gmail.com>
2014-06-27 19:55:17 +0200

    Add mutex to only allow one program instance

--- o!RA/Program.cs
+++ o!RA/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading;
 using System.Windows.Forms;
 using o_RA.Forms;
 
@@ -6,16 +7,26 @@ namespace o_RA
 {
     static class Program
     {
+        private static readonly Mutex Mutex = new Mutex(false, "o!RA");
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
         [STAThread]
         static void Main()
         {
-            AppDomain.CurrentDomain.UnhandledException += HandleExcep;
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new oRAMainForm());
+            if (!Mutex.WaitOne(TimeSpan.FromSeconds(0), false))
+            {
+                MessageBox.Show(@"o!RA is already running!", "", MessageBoxButtons.OK);
+                return;
+            }
+            try
+            {
+                AppDomain.CurrentDomain.UnhandledException += HandleExcep;
+                Application.EnableVisualStyles();
+                Application.SetCompatibleTextRenderingDefault(false);
+                Application.Run(new oRAMainForm());
+            }
+            finally { Mutex.ReleaseMutex(); }
         }
 
         static void HandleExcep(object sender, UnhandledExceptionEventArgs e)

1e9f3f008fbdb88a646f681e8b74c9a4771a0fa5
smoogipooo <smoogipooo@gmail.com>
2014-09-11 03:24:36 +0930

    Add localizer globalvars.

---
+++ o!RA Locale Generator/GlobalVars.cs
@@ -0,0 +1,14 @@
+﻿namespace o_RA_Locale_Generator
+{
+    class GlobalVars
+    {
+        /// <summary>
+        /// Google username.
+        /// </summary>
+        public static string Username = "";
+        /// <summary>
+        /// Google password/app specific password
+        /// </summary>
+        public static string Password = "";
+    }
+}
